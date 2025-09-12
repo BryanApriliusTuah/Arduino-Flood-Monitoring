1801ff51a3cef2a60e76e93367ab3cab1525710d
Bryan Aprilius Tuah <tuahayen@gmail.com>
2025-09-12 12:53:11 +0700

    Fixing JSON format

--- set_socket.h
+++ set_socket.h
@@ -5,7 +5,7 @@ using namespace websockets;
 WebsocketsClient websocket;
 
 bool websocketConnected = false;
-String socketUrl = "ws://192.168.1.9:8001/ws";
+String socketUrl = "ws://192.168.1.4:8001/ws";
 
 void init_websocket(){
   Serial.println("[Websocket] Connecting to server...");
@@ -32,7 +32,22 @@ void init_websocket(){
   websocket.connect(socketUrl);
 }
 
-void push_websocket(String level, String rainfall, String lat, String lng){
+void push_timeReadySocket(String message){
+  if (!websocketConnected) {
+    websocket.connect(socketUrl);
+  }
+
+  if (!websocket.available()) {
+    Serial.println("[Websocket] Not connected. Skipping send...");
+    return;
+  }
+
+  Serial.println("[Websocket] Sending time ready...");
+  String jsonWebsocket = "{\"type\": \"time\" , \"timeReady\":\"" + message + "\"}";
+  websocket.send(jsonWebsocket);
+}
+
+void push_websocket(String level, String rainfall, String siaga, String banjir, String lat, String lng){
   if (!websocketConnected) {
     websocket.connect(socketUrl);
   }
@@ -43,6 +58,6 @@ void push_websocket(String level, String rainfall, String lat, String lng){
   }
 
   Serial.println("[Websocket] Sending data...");
-  String jsonWebsocket = "{\"hardwareId\": 1 ,\"elevation\":" + level + ",\"curah_hujan\":" + rainfall + ",\"latitude\":\"" + lat + "\",\"longitude\":\"" + lng + "\"}";
+  String jsonWebsocket = "{\"type\": \"data\" ,\"hardwareId\": 1 ,\"elevation\":" + level + ",\"level_siaga\":" + siaga + ",\"level_banjir\":" + banjir + ",\"curah_hujan\":" + rainfall + ",\"latitude\":\"" + lat + "\",\"longitude\":\"" + lng + "\"}";
   websocket.send(jsonWebsocket);
 }
