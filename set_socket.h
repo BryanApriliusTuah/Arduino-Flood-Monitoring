50c448112c022828fadeb1644e1a58127a036011
Bryan Aprilius Tuah <tuahayen@gmail.com>
2025-10-11 01:04:59 +0700

    Add GSM connection for Socket

--- set_socket.h
+++ set_socket.h
@@ -1,63 +1,80 @@
-#include <Arduino.h>
-#include <ArduinoWebsockets.h>
+#pragma once
 
-using namespace websockets;
-WebsocketsClient websocket;
+#include <WebSocketsClient.h> 
+#include "set_internet.h" 
 
+const char* websocket_host = "srv1036121.hstgr.cloud";
+const int websocket_port = 8001;
+const char* websocket_path = "/ws";
+
+WebSocketsClient webSocket;
 bool websocketConnected = false;
-String socketUrl = "ws://192.168.1.4:8001/ws";
 
-void init_websocket(){
-  Serial.println("[Websocket] Connecting to server...");
-
-  websocket.onEvent([](WebsocketsEvent event, String data) {
-    if (event == WebsocketsEvent::ConnectionOpened) {
-      Serial.println("[Websocket] Connection opened");
-      websocketConnected = true;
-    } else if (event == WebsocketsEvent::ConnectionClosed) {
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
       Serial.println("[Websocket] Connection closed");
       websocketConnected = false;
-    } else if (event == WebsocketsEvent::GotPing) {
-      Serial.println("[Websocket] Received Ping");
-    } else if (event == WebsocketsEvent::GotPong) {
-      Serial.println("[Websocket] Received Pong");
-    }
-  });
+      break;
+    case WStype_CONNECTED:
+      Serial.println("[Websocket] Connection opened");
+      websocketConnected = true;
+      break;
+    case WStype_TEXT:
+      Serial.printf("[Websocket] Message received: %s\n", payload);
+      websocketConnected = true;
+      break;
+    case WStype_ERROR:
+      Serial.println("[Websocket] Error occurred");
+      websocketConnected = false;
+      break;
+    default:
+      break;
+  }
+}
 
-  websocket.onMessage([](WebsocketsMessage message) {
-    Serial.print("[Websocket] Message received: ");
-    Serial.println(message.data());
-  });
+void connect_websocket() {
+  Serial.println("[Websocket] Attempting to connect...");
+  webSocket.begin(websocket_host, websocket_port, websocket_path);
+}
 
-  websocket.connect(socketUrl);
+void init_websocket(){
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(5000);  // Reconnect every 5 seconds if disconnected
+  webSocket.enableHeartbeat(15000, 3000, 2);  // ping interval, pong timeout, disconnect after N failures
+  connect_websocket();
 }
 
 void push_timeReadySocket(String message){
   if (!websocketConnected) {
-    websocket.connect(socketUrl);
-  }
-
-  if (!websocket.available()) {
-    Serial.println("[Websocket] Not connected. Skipping send...");
+    Serial.println("[Websocket] Not connected. Skipping time message...");
     return;
   }
-
-  Serial.println("[Websocket] Sending time ready...");
   String jsonWebsocket = "{\"type\": \"time\" , \"timeReady\":\"" + message + "\"}";
-  websocket.send(jsonWebsocket);
+  bool sent = webSocket.sendTXT(jsonWebsocket);
+  if (!sent) {
+    Serial.println("[Websocket] Failed to send time message");
+  }
 }
 
 void push_websocket(String level, String rainfall, String siaga, String banjir, String lat, String lng){
   if (!websocketConnected) {
-    websocket.connect(socketUrl);
-  }
-
-  if (!websocket.available()) {
-    Serial.println("[Websocket] Not connected. Skipping send...");
+    Serial.println("[Websocket] Not connected. Skipping data send...");
     return;
   }
-
-  Serial.println("[Websocket] Sending data...");
-  String jsonWebsocket = "{\"type\": \"data\" ,\"hardwareId\": 1 ,\"elevation\":" + level + ",\"level_siaga\":" + siaga + ",\"level_banjir\":" + banjir + ",\"curah_hujan\":" + rainfall + ",\"latitude\":\"" + lat + "\",\"longitude\":\"" + lng + "\"}";
-  websocket.send(jsonWebsocket);
+  
+  String jsonWebsocket = 
+    "{\"type\": \"data\" ,\"hardwareId\": 1 ,\"elevation\":" + level +
+    ",\"level_siaga\":" + siaga +
+    ",\"level_banjir\":" + banjir +
+    ",\"curah_hujan\":" + rainfall +
+    ",\"latitude\":\"" + lat +
+    "\",\"longitude\":\"" + lng + "\"}";
+  
+  bool sent = webSocket.sendTXT(jsonWebsocket);
+  if (sent) {
+    Serial.println("[Websocket] Data sent successfully");
+  } else {
+    Serial.println("[Websocket] Failed to send data");
+  }
 }
